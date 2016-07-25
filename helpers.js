c4537cfc91f611bca7b1084cad14af93b5507f3c
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-25 14:07:35 -0400

    Pickering Town Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,8 @@
 function init(e){
     $('<div class="modal-backdrop custom_backdrop"><img src="http://assets.codecloudapp.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
 }
+
+function show_content(){
+    $('.custom_backdrop').remove();
+    $('.yield').fadeIn();
+}
