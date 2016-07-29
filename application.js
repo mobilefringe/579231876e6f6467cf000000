a37b6f6f552c8ee5b2e9fb8036895133c29c81bc
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-29 13:13:55 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -31,3 +31,89 @@ function renderBanner(banner_template,home_banner,banners){
     });
     $(home_banner).html(item_rendered.join(''));
 }
+
+function renderHours(container, template, collection, type){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    if (type == "reg_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == false) {
+                switch(val.day_of_week) {
+                    case 0:
+                        val.day = "Sunday";
+                        break;
+                    case 1:
+                        val.day = "Monday";
+                        break;
+                    case 2:
+                        val.day = "Tuesday";
+                        break;
+                    case 3:
+                        val.day = "Wednesday";
+                        break;
+                    case 4:
+                        val.day = "Thursday";
+                        break;
+                    case 5:
+                        val.day = "Friday";
+                        break;
+                    case 6:
+                        val.day = "Saturday";
+                        break;
+                }
+                if (val.open_time && val.close_time && val.is_closed == false){
+                    var open_time = new Date (val.open_time);
+                    var close_time = new Date (val.close_time);
+                    val.open_time = convert_hour(open_time);
+                    val.close_time = convert_hour(close_time);    
+                    val.h = val.open_time+ " - " + val.close_time;
+                } else {
+                    "Closed";
+                }
+                
+                item_list.push(val);
+            }
+        });
+        collection = [];
+        collection = item_list;
+    }
+    
+    if (type == "holiday_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == true) {
+                holiday = new Date (val.holiday_date + "T05:00:00Z");
+                var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                val.formatted_date = get_month(holiday.getMonth()) + " " +holiday.getDate();
+                if (val.open_time && val.close_time && val.is_closed == false){
+                    var open_time = new Date (val.open_time);
+                    var close_time = new Date (val.close_time);
+                    val.open_time = convert_hour(open_time);
+                    val.close_time = convert_hour(close_time);    
+                    if (val.open_time == "0:00 AM"){
+                        val.open_time = "12:00 AM";
+                    }
+                     if (val.close_time == "0:00 AM"){
+                        val.close_time = "12:00 AM";
+                    }
+                    val.h = val.open_time+ " - " + val.close_time;
+                } else {
+                    val.h = "Closed";
+                }
+                item_list.push(val);
+            }
+        });
+        collection = [];
+        collection = item_list;
+    }
+    
+    $.each( collection , function( key, val ) {
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+
+    });
+    
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+};
