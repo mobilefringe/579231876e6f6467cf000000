05d9d3e3c76c414addcdadd05c6f00948d94e12a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-29 13:14:26 -0400

    Pickering Town Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -6,3 +6,46 @@ function show_content(){
     $('.custom_backdrop').remove();
     $('.yield').fadeIn();
 }
+
+function get_day(id){
+    switch(id) {
+        case 0:
+            return ("Sunday");
+            break;
+        case 1:
+            return ("Monday");
+            break;
+        case 2:
+            return ("Tuesday");
+            break;
+        case 3:
+            return ("Wednesday");
+            break;
+        case 4:
+            return ("Thursday");
+            break;
+        case 5:
+            return ("Friday");
+            break;
+        case 6:
+            return ("Saturday");
+            break;
+    }
+}
+
+
+function convert_hour(d){
+    var h = (d.getUTCHours());
+    var m = addZero(d.getUTCMinutes());
+    var s = addZero(d.getUTCSeconds());
+    if (h >= 12) {
+        if ( h != 12) {
+            h = h - 12;    
+        }
+        
+        i = "pm"
+    } else {
+        i = "am"
+    }
+    return h+":"+m+i;
+}
