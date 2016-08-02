cd910304a4dc5b752c64ab43e2853db46a122eab
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-02 13:35:31 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -137,7 +137,7 @@ function renderEvents(container, template, collection){
             val.store_name = "Pickering Town Centre";
         }
         if(val.event_image_url_abs.indexOf('missing.png') >=0){
-            val.logo = "http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/jpeg/1446753494000/Dixie_default.jpg";
+            val.logo = "http://assets.codecloudapp.com/sites/579231876e6f6467cf000000/image/png/1469552090000/PTC-Logo-x2.png";
         }
         var show_date = moment(val.show_on_web_date);
         start = moment(val.start_date);
