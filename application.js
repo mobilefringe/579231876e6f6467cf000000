6dab0662f7f8013b758aecd96e60f0b7bf3501f5
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-02 13:23:25 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -143,9 +143,7 @@ function renderEvents(container, template, collection){
         var show_date = moment(val.show_on_web_date);
         start = moment(val.start_date);
         end = moment(val.end_date);
-        if (start == end){
-            
-        }
+        
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
