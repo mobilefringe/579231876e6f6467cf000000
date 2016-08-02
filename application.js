416084c6d6bddf7071a4a5d883b62f94d777d850
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-02 11:43:46 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -67,7 +67,7 @@ function renderHours(container, template, collection, type){
             if (val.open_time && val.close_time && val.is_closed == false){
                 var open_time = in_my_time_zone(moment(val.open_time), "HH:mm a");
                 var close_time = in_my_time_zone(moment(val.close_time), "HH:mm a");
-                val.h = val.open_time+ " - " + val.close_time;
+                val.h = open_time + " - " + close_time;
             } else {
                 "Closed";
             }
