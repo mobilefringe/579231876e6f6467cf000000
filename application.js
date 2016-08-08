3243bfb28d11a06f4fdfa9bd6f36ef32cb05703b
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-08 15:57:09 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -396,6 +396,19 @@ function renderStoreList(container, template, collection, type, starter, breaker
 }
 
 
+function renderGeneral(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+
 
 
 
