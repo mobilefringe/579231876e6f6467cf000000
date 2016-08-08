7b3d15ebebb55e575d6c6f09b78f9554c45382e1
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-08 15:58:49 -0400

    Pickering Town Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -109,4 +109,47 @@ function isInt(value) {
     return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
 }
 
+function show_cat_stores(){
+    $('.show_cat_stores').click(function(e){
+        var cat_id = $(this).attr('data-id');
+        $('.active_cat').removeClass('active_cat');
+        $(this).addClass('active_cat');
+        var rows = $('.cats_row');
+        rows.hide();
+        $('.store_initial').hide();
+        $('#cat_name').text($(this).text());
+        $('#cat_name').css('display', 'block');
+        $('#store_list_container, #store_list_container2').addClass("full_width");
+        $.each(rows, function(i, val){
+            var cat_array = val.getAttribute('data-cat').split(',');
+            if ($.inArray(cat_id, cat_array) >= 0){
+                $(val).show();
+            }
+        });
+        $('html, body').animate({scrollTop : 0},800);
+        e.preventDefault();
+    });
+    $('.show_all_stores').click(function(e){
+        $('#store_list_container, #store_list_container2').removeClass("full_width");
+        $('.active_cat').removeClass('active_cat');
+        $(this).addClass('active_cat');
+        var rows = $('.cats_row');
+        if ($(window).width() > 768){
+            rows.show();
+        }
+        else{
+            rows.hide();
+        }
+        $.each($('.store_initial'), function(i, val){
+           if ($(val).text().trim().length > 0){
+               $(val).show();
+           } 
+        });
+        
+        $('#cat_name').hide();
+        e.preventDefault();
+    });
+    
+}
+
 
