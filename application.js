c1e0e2048720ed019a92997b39c8234d5798f50b
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-10 11:10:16 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -509,17 +509,8 @@ function renderPosts(container, template, collection){
             val.description_short = val.body;
         }
         val.description_short = val.description_short.replace("&amp;", "&");
-        val.slug = "posts/" +val.slug;
-        var lb = getBlogDataBySlug("stc-lookbook");
-        var contest = getBlogDataBySlug("stc-contest");
-        var out_blog = lb.posts.concat(contest.posts);
-        var id = val.id;
-        var result = $.grep(out_blog, function(e){ return e.id == id; });
-        if(result.length > 0){
-            val.slug = val.video_link;
-        }
-        val.counter = counter;
-        var date_blog = new Date(val.publish_date);
+        
+        var date_blog = moment(val.publish_date);
         val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
         
         var rendered = Mustache.render(template_html,val);
