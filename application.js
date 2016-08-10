9b4473ea25996f2189f54722a46415c83c1afa84
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-10 11:09:51 -0400

    Pickering Town Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -490,6 +490,47 @@ function renderGallery(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+function renderPosts(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    var counter = 1;
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    $.each( collection , function( key, val ) {
+        if (val.image_url.indexOf('missing.png') > -1) {
+            val.post_image = "http://assets.codecloudapp.com/sites/579231876e6f6467cf000000/image/png/1469552090000/PTC-Logo-x2.png";
+        } else {
+            val.post_image = val.image_url;
+        }
+        if(val.body.length > 100){
+            val.description_short = val.body.substring(0,100) + "...";
+        }
+        else{
+            val.description_short = val.body;
+        }
+        val.description_short = val.description_short.replace("&amp;", "&");
+        val.slug = "posts/" +val.slug;
+        var lb = getBlogDataBySlug("stc-lookbook");
+        var contest = getBlogDataBySlug("stc-contest");
+        var out_blog = lb.posts.concat(contest.posts);
+        var id = val.id;
+        var result = $.grep(out_blog, function(e){ return e.id == id; });
+        if(result.length > 0){
+            val.slug = val.video_link;
+        }
+        val.counter = counter;
+        var date_blog = new Date(val.publish_date);
+        val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+        counter = counter+1;
+    });
+    
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+}
+
 
 
 
