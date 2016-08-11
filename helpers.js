f64da1b085de1caddbb347abb2d68d8e25d63b4f
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-11 15:03:02 -0400

    Pickering Town Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -172,3 +172,14 @@ function validateNewsletterForm(){
         return true;
     }
 }
+
+function getUrlVars(){
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++){
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
