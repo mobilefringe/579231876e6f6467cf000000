ad19750fb6e6e3df7ee363d0f8918dc4a72bb8b4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-31 12:29:47 -0500

    Pickering Town Centre 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -233,6 +233,7 @@ function submit_contest(slug) {
     contest_data.phone = $('#phone').val();
     contest_data.email = $('#email').val();
     contest_data.newsletter = $('#newsletter_signup').prop("checked");
+    console.log("retail check", $('#retail_check').prop("checked"))
     contest_data.get_vendor_info = $('#retail_check').prop("checked");
     
     contest_entry.contest = contest_data;
